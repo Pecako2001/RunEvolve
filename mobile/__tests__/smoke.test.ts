172a9f1fd8b2979ffe823eb60dcaaefa60b271df
Koen van Wijlick <koenvanwijlick@gmail.com>
2025-06-02 13:30:35 +0200

    [mobile] replace smoke test with updated testing-library approach

--- mobile/__tests__/smoke.test.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from '../App';
-
-test('renders without crashing', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toBeTruthy();
-});
